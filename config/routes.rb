845a4333cba0ed974ab0ba7e4eab483593d60483
roque <roque.volkmer@gmail.com>
2013-04-18 15:36:09 -0300

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 SampleRv::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
 
-  get "static_pages/help"
+    root to: 'static_pages#home'
 
-  get "static_pages/about"
+  match '/home', to:'static_pages#home'
 
-  get "static_pages/contact"
+  match '/help', to: 'static_pages#help'
+
+  match '/about', to: 'static_pages#about'
+
+  match '/contact', to: 'static_pages#contact'
+
+   match '/signup', to: 'users#new'
+
 end
